ec860ff930a3f6c171753ee8cc0993291502b453
Garvin <gpang@godaddy.com>
2021-01-29 14:38:04 -0800

    add logic to find stack with name

--- pkg/aws/services/cloudformation.go
+++ pkg/aws/services/cloudformation.go
@@ -13,12 +13,13 @@ type Cloudformation interface {
 	cloudformationiface.CloudFormationAPI
 
 	// wrapper for ListStackInstancesWithContext and aggregate result into list
-	ListStackAsList(ctx context.Context, stackStatus []*string) ([]*cloudformation.StackInstanceSummary, error)
+	ListStackAsList(ctx context.Context, stackStatus []*string) ([]*cloudformation.StackSummary, error)
 
 	// wrapper for DescribeStackEvents and aggregate result into list
 	DescribeStackEventsAsList(ctx context.Context, stackName string) ([]*cloudformation.StackEvent, error)
 }
 
+//NewCloudFormation creates a new CloudFormatinon wrapper
 func NewCloudFormation(session *session.Session) Cloudformation {
 	return &defaultCloudformation{
 		CloudFormationAPI: cloudformation.New(session),
@@ -29,15 +30,15 @@ type defaultCloudformation struct {
 	cloudformationiface.CloudFormationAPI
 }
 
-func (c *defaultCloudformation) ListStackAsList(ctx context.Context, stackStatus []*string) ([]*cloudformation.StackInstanceSummary, error) {
-	var result []*cloudformation.StackInstanceSummary
+func (c *defaultCloudformation) ListStackAsList(ctx context.Context, stackStatus []*string) ([]*cloudformation.StackSummary, error) {
+	var result []*cloudformation.StackSummary
 
 	input := cloudformation.ListStacksInput{}
 
 	input.SetStackStatusFilter(stackStatus)
 
-	if err := c.ListStackInstancesPagesWithContext(ctx, &input, func(output *cloudformation.ListStackInstancesOutput, _ bool) bool {
-		result = append(result, output.Summaries...)
+	if err := c.ListStacksPagesWithContext(ctx, &input, func(output *cloudformation.ListStacksOutput, _ bool) bool {
+		result = append(result, output.StackSummaries...)
 		return true
 	}); err != nil {
 		return nil, err
--- pkg/ctl/find.go
+++ pkg/ctl/find.go
@@ -3,22 +3,28 @@ package ctl
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 
+	"cloudformation-error/pkg/aws/model/cloudformation"
 	"cloudformation-error/pkg/aws/services"
 )
 
 func Find(ctx context.Context, stackName string, region string) string {
 	awsSess := session.Must(session.NewSession(&aws.Config{Region: aws.String(region)}))
 	cfClient := services.NewCloudFormation(awsSess)
-	result, err := cfClient.ListStackAsList(ctx, "input")
+	create := cloudformation.StackStatusUPDATEROLLBACKCOMPLETE.String()
+	stackStatus := []*string{&create}
+	result, err := cfClient.ListStackAsList(ctx, stackStatus)
+	for _, summary := range result {
+		if strings.Contains(*summary.StackName, stackName) {
+			return *summary.StackName
+		}
+	}
 	if err != nil {
 		log.Fatal(err)
-	}
-	for _, r := range result {
-		print(r.StackId)
 	}
 	return "sdf"
 }
